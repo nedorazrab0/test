81a324b3239b8bab3b1d4b6a881bfe9b237f3867
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-20 20:32:38 +0300

    Update hui.sh

--- hui.sh
+++ hui.sh
@@ -1,15 +1,17 @@
-#sudo apt update
-sudo apt install apktool -y
+link="$(curl -fs https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"			 
+curl -fsLo euapp.apk "$link"                                                                                                                                                                                                  
+sudo apt install apktool -y                                                                                                                                                                                                   
+apktool d -fsb --no-assets ./euapp.apk                                                                                                                                                                                        
+                                                                                                                                                                                                                              
+echo '{' > pif.json                                                                                                                                                                                                           
+for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do                                                                                                                             
+    key="$(grep "$val" ./euapp.out/res/xml/inject_fields.xml | awk -F 'value=' '{print $2}' | awk '{print $1}')"                                                                                                                  
+    echo "\"$val\": ${key:-\"null\"}" >> pif.json                                                                                                                                                                                 
+done                                                                                                                                                                                                                          
+echo '}' >> pif.json 
 
-echo '{' > huj
-for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
-key="$(grep "$val" ./res/xml/inject_fields.xml | awk -F 'value=' '{print $2}' | awk '{print $1}')"
-echo "\"$val\": ${key:-\"null\"}" >> huj
-done
-echo '}' >> huj
-
-git config --global user.name "$1"
-git config --global user.email "username@users.noreply.github.com"
-git add huj
+git config --global user.name 'github-actions'
+git config --global user.email 'github-actions@github.com'
+git add pif.json
 git commit -m "hz"
 git push
