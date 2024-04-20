bf4cb39074f7205f81b3983aefb75b3726b9a821
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-20 17:55:17 +0300

    Update hui.sh

--- hui.sh
+++ hui.sh
@@ -1,1 +1,12 @@
-echo 76 > huj
+echo '{' > huj
+for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
+key="$(grep "$val" ./res/xml/inject_fields.xml | awk -F 'value=' '{print $2}' | awk '{print $1}')"
+echo "\"$val\": ${key:-\"null\"}" >> huj
+done
+echo '}' >> huj
+
+git config --global user.name "$1"
+git config --global user.email "username@users.noreply.github.com"
+git add huj
+git commit -m "hz"
+git push
