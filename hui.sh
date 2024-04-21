52e71efc727362d68a07febbf0a5c0f0e6bf5828
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-21 11:58:48 +0300

    Update hui.sh

--- hui.sh
+++ hui.sh
@@ -1,13 +1,3 @@
-echo
-uname -a
-echo
-cat /proc/cpuinfo
-echo
-free -h
-echo
-lsblk
-echo
-echo
 link="$(curl -fs https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"
 curl -fsLo ./euapp.apk "$link"
 sudo apt install apktool -y
@@ -17,7 +7,7 @@ echo '{' > pif.json
 for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
     key="$(grep "$val" ./euapp/res/xml/inject_fields.xml | awk -F 'value=' '{print $2}' | awk '{print $1}')"
     echo "\"$val\": ${key:-\"null\"}," >> pif.json
-    [[ "$val" == 'FIRST_API_LEVEL' ]] && sed -n '2s/.$//'
+    [[ "$val" == 'FIRST_API_LEVEL' ]] && sed -n '9s/.$//'
 done
 echo '}' >> pif.json
 
