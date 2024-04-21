f2d045afcd45b78276b60079107ddcbb51e5f285
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-21 12:26:11 +0300

    Update hui.sh

--- hui.sh
+++ hui.sh
@@ -3,7 +3,7 @@ mkdir -p /tmp/apif/
 cd /tmp/apif/
 link="$(curl -fs https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"
 curl -fsLo ./euapp.apk "$link"
-sudo apt install apktool -y
+sudo apt-cache install apktool -y
 apktool d --no-assets -fsbo ./euapp/ ./euapp.apk
 
 echo '{' > pif.json
