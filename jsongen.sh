8f946d06127c57c9103147d891de7e3c225d8260
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-22 11:44:24 +0300

    Update jsongen.sh

--- jsongen.sh
+++ jsongen.sh
@@ -1,7 +1,9 @@
+# get link & download apk
 link="$(curl -fs https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"
 curl -fsLo ./euapp.apk "$link"
 apktool d --no-assets -fsbo ./euapp/ ./euapp.apk
 
+# generate pif.json
 echo '{' > pif.json
 for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
     key="$(grep "$val" ./euapp/res/xml/inject_fields.xml | awk -F 'value=' '{print $2}' | awk '{print $1}')"
@@ -10,6 +12,7 @@ for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FI
 done
 echo '}' >> pif.json
 
+# release pif.json
 git config --global user.name 'github-actions'
 git config --global user.email 'github-actions@github.com'
 git add pif.json
