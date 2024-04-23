1079d5ba1719c4b6addd93ce585ffc703a0f0a8c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-23 18:04:32 +0300

    Update track_link.sh

--- track_link.sh
+++ track_link.sh
@@ -1,4 +1,4 @@
-while [[ "$SECONDS" -lt '21570' ]]; do
+while [[ "$SECONDS" -lt '3600' ]]; do
     link="$(curl -fs 'https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app' | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"
     if ! [[ "$(cat ./link.txt)" == "$link" ]]; then
         echo "$link" > ./link.txt
