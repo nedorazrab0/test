39e3341e0e0a31dfe9c3798322b83241ea99e9d6
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-23 15:59:03 +0300

    Create track_link.sh

---
+++ track_link.sh
@@ -0,0 +1,8 @@
+while [[ "$SECONDS" -lt '21570' ]]; do
+    link="$(curl -fs 'https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app' | awk '/link/ && /app/' | sed -n 1p | awk -F '>' '{print $2}' | awk -F '<' '{print $1}')"
+    if ! [[ "$(cat ./link.txt)" == "$link" || -f ./link.txt ]]; then
+        echo "$link" > ./link.txt
+        source ./json_generate.sh
+    fi
+    sleep 10
+done
