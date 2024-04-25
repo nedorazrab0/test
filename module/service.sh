035635c377447a074f102ae67088be7ffb645a8a
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-04-25 21:22:01 +0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,19 +1,9 @@
-MODDIR=${0%/*}
-get_json(){
-    curl -fs 'https://raw.githubusercontent.com/nedorazrab0/test/main/pif.json' "$@"
-}
-
-until ping -c 1 google.com; do
-    sleep 1
-done
-
 while true; do
-    until get_json; do
-        sleep 10
+    until ping -qns1 -c1 github.com; do
+        sleep 1
     done
 
-    get_json -o "$MODDIR/pif.json"
-    cp -f "$MODDIR/pif.json" /data/adb/
+    curl -fso /data/adb/pif.json 'https://raw.githubusercontent.com/nedorazrab0/test/main/pif.json'
     pkill -f com.google.android.gms
     sleep 43200
 done
