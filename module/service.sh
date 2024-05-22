4696a8ec907f78aa5046699bf556fbec23b64b13
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-22 18:07:17 +0300

    Delete module directory

--- module/service.sh
+++
@@ -1,9 +0,0 @@
-while true; do
-    until ping -qns1 -c1 github.com; do
-        sleep 1
-    done
-
-    curl -fso /data/adb/pif.json 'https://raw.githubusercontent.com/nedorazrab0/test/main/pif.json'
-    pkill -f com.google.android.gms
-    sleep 43200
-done
