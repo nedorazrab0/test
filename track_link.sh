d2357c2ef7650452b46fce9812edee0ae073107c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-23 11:20:19 +0300

    Update track_link.sh

--- track_link.sh
+++ track_link.sh
@@ -1,3 +1,7 @@
+sudo apt update
+apt search aapt
+echo hui
+
 #while true; do
 link="$(curl -fs 'https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app' | grep -om1 'https.*download')"
 if [[ "$link" ]]; then
