fc59b0d91639cc2e5f3487a7e20c617a553b2064
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-23 17:30:56 +0300

    nahui

--- generate_json.sh
+++ generate_json.sh
@@ -8,11 +8,23 @@ aapt dump xmltree euapp.apk res/xml/inject_fields.xml > ./hui.xml
 echo '{' > pif.json
 for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
     key="$(grep -A2 "$val" ./hui.xml | sed -n 3p | awk -F 'value=' '{print $2}' | awk '{print $1}')"
-    echo "\"$val\": ${key:-\"null\"}," >> pif.json
-    [[ "$val" == 'FIRST_API_LEVEL' ]] && sed -i '9s/.$//' ./pif.json
+    echo "$val ${key:-null}" >> pif.json
 done
-echo '}' >> pif.json
+echo >> pif.json
 
+echo 'TYPE user' >> ./pif.json
+echo 'TAGS release-keys' >> ./pif.json
+echo "ID $(grep 'FINGERPRINT' ./pif.json | awk -F '/' '{print $4}')" >> ./pif.json
+echo "INCREMENTAL $(grep 'FINGERPRINT' ./pif.json | awk -F '/' '{print $5}' | awk -F ':' '{print $1}')"
+echo "RELEASE $(grep 'FINGERPRINT' ./pif.json | awk -F '/' '{print $3}' | awk -F ':' '{print $2}')"
+if [[ "$(grep 'SECURITY_PATCH' ./pif.json | awk '{print $2}')" == 'null' ]]; then
+    echo 'DEVICE_INITIAL_SDK_INT 25' >> ./pif.json
+else
+    echo "DEVICE_INITIAL_SDK_INT $(grep 'SECURITY_PATCH' ./pif.json | awk '{print $2}')" >> pif.json
+fi
+
+
+#sed -i '$s/.$//' ./pif.json
 # release pif.json
 git config --global user.name 'github-actions'
 git config --global user.email 'github-actions@github.com'
