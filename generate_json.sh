26ac799aab9345378f099f20189c0026e75ae0ea
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-26 17:53:59 +0300

    Update generate_json.sh

--- generate_json.sh
+++ generate_json.sh
@@ -6,12 +6,10 @@ curl -fsZLo ./euapp.apk "$link"
 
 aapt dump xmltree euapp.apk res/xml/inject_fields.xml > ./hui.xml
 # generate json
-echo '{' > pif.json
 for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
     key="$(grep -A2 "$val" ./hui.xml | sed -n 3p | awk -F \" '{print $2}')"
     echo "$val ${key:-null}" >> pif.json
 done
-echo >> pif.json
 
 echo 'TYPE user' >> ./pif.json
 echo 'TAGS release-keys' >> ./pif.json
@@ -21,10 +19,11 @@ echo "RELEASE $(grep 'FINGERPRINT' ./pif.json | awk -F '/' '{print $3}' | awk -F
 if [[ "$(grep 'SECURITY_PATCH' ./pif.json | awk '{print $2}')" == 'null' ]]; then
     echo 'DEVICE_INITIAL_SDK_INT 25' >> ./pif.json
 else
-    echo "DEVICE_INITIAL_SDK_INT $(grep 'SECURITY_PATCH' ./pif.json | awk '{print $2}')" >> pif.json
+    echo "DEVICE_INITIAL_SDK_INT $(grep 'FIRST_API_LEVEL' ./pif.json | awk '{print $2}')" >> pif.json
 fi
 
-awk -i inplace '{printf "\"%s\": \"%s\"\n", $1, $2}' ./pif.json
+awk -i inplace '{printf "\"%s\": \"%s\"\,\n", $1, $2}' ./pif.json
+sed -i '1i\{'
 sed -i '$s/.$//' ./pif.json
 echo '}' >> ./pif.json
 # release pif.json
