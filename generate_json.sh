6de366415a18ac3389378b55ccf9c83d4ec5a494
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-28 11:44:53 +0300

    Update generate_json.sh

--- generate_json.sh
+++ generate_json.sh
@@ -12,19 +12,19 @@ for val in BRAND MANUFACTURER PRODUCT DEVICE MODEL SECURITY_PATCH FINGERPRINT; d
     echo "$val ${key:-null}" >> pif.json
 done
 
+fp="$(grep 'FINGERPRINT' ./pif.json)"
+echo "RELEASE $(echo $fp ./pif.json | awk -F '/' '{print $3}' | awk -F ':' '{print $2}')" >> ./pif.json
+echo "INCREMENTAL $(echo $fp | awk -F '/' '{print $5}' | awk -F ':' '{print $1}')" >> ./pif.json
+echo "ID $(echo $fp | awk -F '/' '{print $4}')" >> ./pif.json
+echo "TYPE $(echo $fp | awk -F '/' '{print $5}' | awk -F ':' '{print $2}')" >> ./pif.json
+echo "TAGS $(echo $fp | awk -F '/' '{print $6}')" >> ./pif.json
+
 apilvl="$(grep -A2 "FIRST_API_LEVEL" ./hui.xml | sed -n 3p | awk -F \" '{print $2}')"
 if [[ -z "$apilvl" ]]; then
     echo 'DEVICE_INITIAL_SDK_INT 25' >> ./pif.json
 else
     echo "DEVICE_INITIAL_SDK_INT $apilvl" >> pif.json
 fi
-
-fp="$(grep 'FINGERPRINT' ./pif.json)"
-echo "TYPE $(echo $fp | awk -F '/' '{print $5}' | awk -F ':' '{print $2}')" >> ./pif.json
-echo "TAGS $(echo $fp | awk -F '/' '{print $6}')" >> ./pif.json
-echo "ID $(echo $fp | awk -F '/' '{print $4}')" >> ./pif.json
-echo "INCREMENTAL $(echo $fp | awk -F '/' '{print $5}' | awk -F ':' '{print $1}')" >> ./pif.json
-echo "RELEASE $(echo $fp ./pif.json | awk -F '/' '{print $3}' | awk -F ':' '{print $2}')" >> ./pif.json
 
 awk -i inplace '{printf "    \"%s\": \"%s\"\,\n", $1, $2}' ./pif.json
 sed -i '1i\{' ./pif.json
