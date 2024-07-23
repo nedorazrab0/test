957d1015149bc5af203372d1bcecabd43aed1724
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-07-23 20:47:17 +0300

    Add files via upload

---
+++ inchroot.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo 'arch' > /etc/hostname
+locale-gen
+ln -sf "/usr/share/zoneinfo/$zone" /etc/localtime
+hwclock --systohc
+useradd -mg users -G wheel "$name"
+echo "$password" | passwd --stdin "$name"
+
+pacman -Syu --noconfirm
+pacman -S opendoas networkmanager network-manager-applet git bash-completion \
+          vulkan-radeon libva-mesa-driver \
+          gdm gnome grub efibootmgr --noconfirm
+
+systemctl enable NetworkManager gdm
+echo 'permit persist :wheel as root' > /etc/doas.conf
+chmod 400 /etc/doas.conf
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+read -p '- Username: ' name
+read -p '- Password: ' password
+read -p '- Timezone (Europe/Moscow):' zone
+read -p '- Country ISO code (for mirrors): ' loc
+read -p '- раскладка (ru_RU)' kblr
+read -p '- Do you want to destroy your own disk? (y/n): ' agreement
+
+case "$agreement" in
+    y) true;;
+    n) exit 0;;
+    *) exit 1;;
+esac
+
+lsblk -do name,model,size,rm,tran
+read -p '- Disk name: ' disk
+sleep 1
+
+blk="/dev/$disk"
+wipefs --all "$blk"
+parted -s "$blk" mktable gpt
+parted -s "$blk" mkpart 'esp' fat32 1Mib 33Mib
+parted -s "$blk" mkpart 'boot' ext4 33Mib 1057MiB
+parted -s "$blk" mkpart 'arch' f2fs 1057MiB 100%
+parted -s "$blk" set 1 boot on
+
+mkdir -p /mnt/eai/
+mount "${blk}*3" /mnt/eai/
+mkdir -p /mnt/eai/boot/
+mount "${blk}*2" /mnt/eai/boot/
+mkdir -p /mnt/eai/boot/efi/
+mount "${blk}*1" /mnt/eai/boot/efi/
+
+sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Colors/Colors/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
+
+pacman -Syy
+pacman -S pacman-contrib --noconfirm
+curl "https://archlinux.org/mirrorlist/?country=${loc}&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -wn 6 - > /var/eai/mirrorlist
+cat /var/eai/mirrorlist > /etc/pacman.d/mirrorlist
+
+pacstrap -K base linux-lts linux-firmware amd-ucode
+
+kbl="${kblr}.UTF-8 UTF-8"
+sed -i -e "s/#$kbl/$kbl/" /mnt/eai/etc/locale.gen
+genfstab -Up /mnt/eai/ > /mnt/eai/etc/fstab
+
+curl -o /var/eai/inchroot.sh https://raw.githubusercontent.com/nedorazrab0/test/main/inchroot.sh
+arch-chroot /mnt/eai/ bash /var/eai/inchroot.sh
+
+echo 'Goodbye ;)'
+umount -R /mnt/eai/
+sleep 1
+poweroff
