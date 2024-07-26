c5f18b730ff85a19b0326f3609dcd99e8e6dce23
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-07-26 23:07:31 +0300

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
-echo CHROOT
-read
+echo '- CHROOT'
 
-zone=Europe/Moscow
-name=hui
-password=123
-
-echo 'arch' > /etc/hostname
 locale-gen
-ln -sf "/usr/share/zoneinfo/$zone" /etc/localtime
+ln -sf "/usr/share/zoneinfo/$3" /etc/localtime
 hwclock --systohc
-useradd -mg users -G wheel "$name"
-echo "$password" | passwd --stdin "$name"
-
+useradd -mg users -G wheel "$1"
+echo "$2" | passwd --stdin "$1"
 pacman -Syu --noconfirm
-pacman -S opendoas networkmanager network-manager-applet git bash-completion \
+pacman -S android-tools android-udev opendoas networkmanager network-manager-applet git bash-completion flatpak \
           vulkan-radeon libva-mesa-driver \
           gdm gnome grub efibootmgr --noconfirm
 
 systemctl enable NetworkManager gdm
-echo 'permit persist :wheel as root' > /etc/doas.conf
-chmod 400 /etc/doas.conf
-grub-install /dev/vda
+grub-install --efi-directory=/boot/efi --target=x86_64-efi
 grub-mkconfig -o /boot/grub/grub.cfg
-
-exit
