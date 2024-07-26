582f142566ffad682c01cae8842907af7f702a05
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-07-26 23:29:46 +0300

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -7,11 +7,13 @@ hwclock --systohc
 useradd -mg users -G wheel "$1"
 echo "$2" | passwd --stdin "$1"
 pacman -Syu --noconfirm
-pacman -S android-tools android-udev opendoas networkmanager network-manager-applet git bash-completion flatpak \
+pacman -S android-tools android-udev opendoas networkmanager network-manager-applet git bash-completion flatpak zram-generator micro \
           vulkan-radeon libva-mesa-driver \
           gdm gnome grub efibootmgr --noconfirm
 
 pacman -R totem gnome-tour --noconfirm
 systemctl enable NetworkManager gdm
+systemctl disable avahi-daemon
+systemctl mask avahi-daemon
 grub-install --efi-directory=/boot/efi --target=x86_64-efi
 grub-mkconfig -o /boot/grub/grub.cfg
