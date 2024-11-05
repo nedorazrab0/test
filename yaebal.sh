173630ecbe2eda5a622544cb2d6e22057b37bd6c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-05 11:28:30 +0300

    Update

---
+++ yaebal.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p /dev/shm/hui
+cd /dev/shm/hui
+
+dd if=/dev/urandom of=z bs=1G conv=fsync
+
+time -p echo z | cpio -oH odc > zn
+time -p echo z | cpio -oH bin > zn
+time -p echo z | cpio -oH newc > zn
+time -p echo z | cpio -oH crc > zn
+
+time -p tar -H v7 -cf zn z
+time -p tar -H oldgnu -cf zn z
+time -p tar -H ustar -cf zn z
+time -p tar -H gnu -cf zn z
+time -p tar -H pax -cf zn z
