008df60cf991025de8966a22fed36fce0672ea46
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-10 12:35:11 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -6,21 +6,9 @@ cd ./z
 curl -ZLo 'hui' 'https://geo.mirror.pkgbuild.com/iso/2024.11.01/archlinux-2024.11.01-x86_64.iso'
 echo '****'
 
-echo '- lz1'
-time -p lz4 -1zc 'hui' > lz1
-echo '- lz3'
-time -p lz4 -3zc 'hui' > lz3
-echo '- lz6'
-time -p lz4 -6zc 'hui' > lz6
-echo '- lz9'
-time -p lz4 -9zc 'hui' > lz9
 echo '- lz12'
 time -p lz4 -12zc 'hui' > lz12
 
-echo '- zp6'
-time -p zip -6 - 'hui' > zp6
-echo '- zp8'
-time -p zip -8 - 'hui' > zp8
 echo '- zp9'
 time -p zip -9 - 'hui' > zp9
 
@@ -31,8 +19,6 @@ time -p 7za a za8 'hui' -mx8
 echo '- za9'
 time -p 7za a za9 'hui' -mx9
 
-echo '- zt1'
-time -p zstd -T0 --ultra -1zc 'hui' > zt1
 echo '- zt3'
 time -p zstd -T0 --ultra -3zc 'hui' > zt3
 echo '- zt6'
@@ -51,3 +37,5 @@ time -p xz -8zekc -T0 -M100% 'hui' > xz9
 
 echo '****'
 ls -lh
+echo '****'
+ls -l
