bdb67a00b32511e78e17688d107cb41354494a1b
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-10 18:54:41 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -6,13 +6,7 @@ cd ./z
 curl -ZLo 'hui' 'https://geo.mirror.pkgbuild.com/iso/2024.11.01/archlinux-2024.11.01-x86_64.iso'
 echo '****'
 
-time -p zip -9 - hui > zp
 time -p 7za a -tzip za 'hui' -mx7
-
-cd ..
-echo zp
-time -p unzip z/zp
-sleep 1
 
 echo za
 time -p 7za x z/za.zip
