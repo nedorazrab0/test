388050aef2571925fe5ee3b656c6e348abfd1258
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-10 12:09:14 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -3,51 +3,51 @@
 mkdir ./z
 cd ./z
 
-curl -ZLo 'hui' 'https://download.fedoraproject.org/pub/fedora/linux/releases/41/Everything/x86_64/iso/Fedora-Everything-netinst-x86_64-41-1.4.iso'
-
-echo '- xz9'
-time -p xz -9zekvc -T0 -M100% 'hui' > xz9
-echo '- xz8'
-time -p xz -8zekvc -T0 -M100% 'hui' > xz8
-echo '- xz6'
-time -p xz -6zekvc -T0 -M100% 'hui' > xz6
-
-echo '- zt22'
-time -p zstd -T0 --ultra -22zvc 'hui' > zt22
-echo '- zt20'
-time -p zstd -T0 --ultra -20zvc 'hui' > zt20
-echo '- zt3'
-time -p zstd -T0 --ultra -3zvc 'hui' > zt3
-echo '- zt1'
-time -p zstd -T0 --ultra -1zvc 'hui' > zt1
-
-echo '- lz12'
-time -p lz4 -12zvc 'hui' > lz12
-echo '- lz9'
-time -p lz4 -9zvc 'hui' > lz9
-echo '- lz6'
-time -p lz4 -6zvc 'hui' > lz6
-echo '- lz3'
-time -p lz4 -6zvc 'hui' > lz3
-echo '- lz1'
-time -p lz4 -1zvc 'hui' > lz1
-
-
-echo '- zp9'
-time -p zip -9v - 'hui' > zp9
-echo '- zp8'
-time -p zip -8v - 'hui' > zp8
-echo '- zp6'
-time -p zip -v - 'hui' > zp6
-
-echo '- za9'
-time -p 7za a - 'hui' -mx9 > za9
-echo '- za8'
-time -p 7za a - 'hui' -mx8 > za8
-echo '- za6'
-time -p 7za a - 'hui' -mx6 > za6
-
+curl -ZLo 'hui' 'https://geo.mirror.pkgbuild.com/iso/2024.11.01/archlinux-2024.11.01-x86_64.iso'
 echo '****'
-ls -l
+
+echo '- lz1'
+time -p lz4 -1zc 'hui' > lz1
+echo '- lz3'
+time -p lz4 -3zc 'hui' > lz3
+echo '- lz6'
+time -p lz4 -6zc 'hui' > lz6
+echo '- lz9'
+time -p lz4 -9zc 'hui' > lz9
+echo '- lz12'
+time -p lz4 -12zc 'hui' > lz12
+
+echo '- zp6'
+time -p zip -6 - 'hui' > zp6
+echo '- zp8'
+time -p zip -8 - 'hui' > zp8
+echo '- zp9'
+time -p zip -9 - 'hui' > zp9
+
+echo '- za6'
+time -p 7za a za6 'hui' -mx6
+echo '- za8'
+time -p 7za a za8 'hui' -mx8
+echo '- za9'
+time -p 7za a za9 'hui' -mx9
+
+echo '- zt1'
+time -p zstd -T0 --ultra -1zc 'hui' > zt1
+echo '- zt3'
+time -p zstd -T0 --ultra -3zc 'hui' > zt3
+echo '- zt6'
+time -p zstd -T0 --ultra -6zc 'hui' > zt6
+echo '- zt20'
+time -p zstd -T0 --ultra -20zc 'hui' > zt20
+echo '- zt22'
+time -p zstd -T0 --ultra -22zc 'hui' > zt22
+
+echo '- xz6'
+time -p xz -6zekc -T0 -M100% 'hui' > xz6
+echo '- xz8'
+time -p xz -8zekc -T0 -M100% 'hui' > xz8
+echo '- xz9'
+time -p xz -8zekc -T0 -M100% 'hui' > xz9
+
 echo '****'
 ls -lh
