a2d5ce455585bfe3afa3d43f12f474569542805b
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 01:13:34 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -5,11 +5,11 @@ mkdir z
 cd z
 sudo apt install libarchive-tools
 
-for i in {0..9}; do
-    dd if=/dev/urandom of=$1 bs=128M count=1 conv=fsync
+for i in $(seq 10); do
+    dd if=/dev/urandom of="$1" bs=128M count=1 conv=fsync
 done
 
-time -p tar -b1 -H ustar -cvf - {0..9} > tr
+time -p tar -b1 -H ustar -cvf - {1..10} > tr
 sleep 1
-time -p bsdtar -b1 -cv {0..9} > btr
+time -p bsdtar -b1 -cv {1..10} > btr
 sleep 1
