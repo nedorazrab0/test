2a3a0b9b088b5abf036c31e987df3afae1b68fdf
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 01:08:59 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set +e
 
-
+mkdir z
+cd z
 sudo apt install libarchive-tools
-bsdtar --version
-command -v bsdtat
-ls -lhL $(which bsdtar)
+
+for i in {0..9}; do
+    dd if=/dev/urandom bs=128M count=1 conv=fsync
+done
+
+time -p tar -b1 -H ustar -cvf - {0..9} > tr
+sleep 1
+time -p bsdtar -b1 -cv {0..9} > btr
+sleep 1
