4e12ffa43d5fd01ac7e68658e2b3f405752c5e70
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 00:21:51 +0300

    Update yaebal.sh

--- yaebal.sh
+++ yaebal.sh
@@ -1,38 +1,6 @@
 #!/usr/bin/env bash
+set +e
 
-mkdir ./z
-cd ./z
-
-curl -ZLo 'hui' 'https://mirrorbits.lineageos.org/full/lisa/20241105/boot.img'
-echo '****'
-
-echo '- tv7'
-time -p tar -H v7 -cf - hui > tv7
-sleep 1
-
-echo '- tps'
-time -p tar -H pax -cf - hui > tps
-sleep 1
-
-echo '- lz1'
-time -p lz4 -1zc tps > lz1
-sleep 1
-
-echo '-lz12'
-time -p lz4 -12zc tps > lz12
-sleep 1
-
-echo '- zt20'
-time -p zstd -T0 --ultra -20zc tps > zt20
-sleep 1
-
-echo '- zt22'
-time -p zstd -T0 --ultra -22zc tps > zt22
-sleep 1
-
-echo '- xz'
-time -p xz -9zekc -T0 -M100% tps > xz
-sleep 1
-
-echo '****'
-ls -lS --block-size=M
+apt search bsdtar
+bsdtar --version
+command -v bsdtat
