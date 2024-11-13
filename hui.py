4529c49c0defe52ca312e9c8150946b648156a95
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 23:37:34 +0300

    Update hui.py

--- hui.py
+++ hui.py
@@ -4,9 +4,9 @@ import hashlib
 
 def csha256(file_path):
     sha256_hash = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(524288), b""):
+    with open(file_path, "rb", buffering=0) as f:
+        for byte_block in iter(lambda: f.read(1048576), b""):
             sha256_hash.update(byte_block)
-    return sha256_hash.hexdigest()
+    return print(sha256_hash.hexdigest())
 
 csha256("zx")
