6febe8dcf9ec8750b9849b8ed2e59ca7bce6f86f
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 23:28:39 +0300

    Update hui.py

--- hui.py
+++ hui.py
@@ -5,6 +5,5 @@ from hashlib import sha256
 file = "zx"
 sha = sha256()
 with open(file, "rb", buffering=0) as f:
-    while sha := f.readinto(65536):
-        sha.update(ch)
+    f.read()
     print(sha.hexdigest())
