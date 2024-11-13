91a8158507ba77be7c3a871ac320dee0d3393aab
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 23:06:06 +0300

    Update hui.py

--- hui.py
+++ hui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import hashlib
+from hashlib import file_digest
 
 with open("zx", 'rb') as x:
-    print(hashlib.file_digest(x, 'sha256').hexdigest())
+    print(file_digest(x, 'sha256').hexdigest())
