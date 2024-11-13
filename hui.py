49905510d790e4b596889175f0f63c0747f9d581
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 23:14:33 +0300

    Update hui.py

--- hui.py
+++ hui.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-from hashlib import sha256
-
-with open("./zx", "rb") as f:
-    f.read()
-    print(sha256(f).hexdigest)
+import hashlib
+# SUKA EBANIY PITON
+filename = "zx"
+sha256_hash = hashlib.sha256()
+with open(filename,"rb") as f:
+    # Read and update hash string value in blocks of 4K
+    for byte_block in iter(lambda: f.read(4096),b""):
+        sha256_hash.update(byte_block)
+    print(sha256_hash.hexdigest())
