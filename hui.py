5844425156337aff49fec8cab3e323a63892cf27
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-11-13 23:15:31 +0300

    Update hui.py

--- hui.py
+++ hui.py
@@ -6,6 +6,6 @@ filename = "zx"
 sha256_hash = hashlib.sha256()
 with open(filename,"rb") as f:
     # Read and update hash string value in blocks of 4K
-    for byte_block in iter(lambda: f.read(4096),b""):
+    for byte_block in iter(lambda: f.read(65536),b""):
         sha256_hash.update(byte_block)
     print(sha256_hash.hexdigest())
