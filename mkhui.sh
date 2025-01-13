c6eb178d12181bc9827087c3e8a0ab3f60a7b757
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-13 17:22:40 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -5,8 +5,8 @@ set -e
 pacman -Sy erofs-utils arch-install-scripts dosfstools xorriso --noconfirm
 
 
-mkdir /hh /hhh /out
-pacstrap -cMG /hh base linux mkinitcpio-archiso &>/dev/null
+mkdir -p /hh/so /hhh /out /hh/iso/
+pacstrap -cMG /hh/so base linux mkinitcpio-archiso &>/dev/null
 
 # ESP
 #espsize="$(du --block-size=1024 -cs /boot | tail -n1 | awk '{print $1}')"
@@ -19,15 +19,16 @@ mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
 cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi /mnt/EFI/BOOT/BOOTx64.EFI
 cat << EOF > /mnt/loader/entries/a.conf
 title a
-linux /vmlinux-linux
-initrd /initramfs-linux.img
-options archisobasedir=/ archisosearchuuid=6b012502-9b5a-45bd-a94e-146f91f98fe5
+linux /hh/so/boot/vmlinux-linux
+initrd /hh/so/boot/initramfs-linux.img
+options archisobasedir=/hh/so archisolabel=HUI
 EOF
 
-mv /hh/boot/* /mnt
+mv /hh/so/boot/* /mnt
 umount /mnt
 
 iso_uuid=6b012502-9b5a-45bd-a94e-146f91f98fe5
 #
-mkfs.erofs -zlz4 -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hhh/airootfs.erofs /hh
-xorriso -no_rc -as mkisofs -iso-level 3 -rational-rock -volid NAME -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hh
+mkfs.erofs -zlz4 -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hh/iso/airootfs.erofs /hh/so
+xorriso -no_rc -as mkisofs -iso-level 3 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hh/iso/
+blkid /out/archiso-v-x86_64.iso
