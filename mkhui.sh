c680d7ad69377e8d9f757eac9aa87eb517a43bfd
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-13 18:01:35 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -18,16 +18,16 @@ mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
 cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi /mnt/EFI/BOOT/BOOTx64.EFI
 cat << 'EOF' > /mnt/loader/entries/a.conf
 title a
-linux /hh/so/boot/vmlinux-linux
-initrd /hh/so/boot/initramfs-linux.img
-options archisobasedir=/hh/so archisolabel=HUI
+linux /so/boot/vmlinux-linux
+initrd /so/boot/initramfs-linux.img
+options archisobasedir=/so archisosearchuuid=6b012502-9b5a-45bd-a94e-146f91f98fe5
 EOF
 
-mv /hh/so/boot/* /mnt
+cp -a /hh/so/boot/* /mnt
 umount /mnt
 
 iso_uuid=6b012502-9b5a-45bd-a94e-146f91f98fe5
 #
-mkfs.erofs -zlz4 -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hh/iso/airootfs.erofs /hh/so
+mkfs.erofs -zlz4 -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hh/iso/airootfs.erofs /hh/
 xorriso -no_rc -as mkisofs -iso-level 3 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hh/iso/
 blkid /out/archiso-v-x86_64.iso
