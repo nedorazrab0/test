64857ddc6262f393c98c07e16fe792056f984da3
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-15 02:16:16 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -9,7 +9,7 @@ export SOURCE_DATE_EPOCH
 
 SOURCE_DATE_EPOCH=$RANDOM
 
-set -e
+set -ex
 pacman -Sy erofs-utils arch-install-scripts dosfstools xorriso --noconfirm
 
 
@@ -27,7 +27,7 @@ pacstrap -cMG /hh/so base linux-zen mkinitcpio mkinitcpio-archiso &>/dev/null
 
 # ESP
 espsize="$(du --block-size=1024 -cs /boot | tail -n1 | awk '{print $1}')"
-mkfs.fat -F16 -S512 -n 'ESP' -C esp.img "${espsize}"
+mkfs.fat -F16 -S512 -s1 -n 'ESP' -C esp.img "${espsize}"
 mount esp.img /mnt
 
 mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
