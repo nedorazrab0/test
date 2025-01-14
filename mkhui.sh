5463c86fdf18b76469e42c53fc6d1c3dc9fb0d6d
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-14 22:28:07 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -26,7 +26,6 @@ pacstrap -cMG /hh/so base linux-zen mkinitcpio mkinitcpio-archiso &>/dev/null
 # ESP
 #espsize="$(du --block-size=1024 -cs /boot | tail -n1 | awk '{print $1}')"
 mkfs.fat -F32 -n 'ESP' -C esp.img 64000  #"${espsize}"
-rm -f /hh/so/boot/*fallback*
 mount esp.img /mnt
 
 mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
