919a98dbb49c15e478309e3f7c8f0ff4e9773b38
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-15 09:38:43 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -50,6 +50,6 @@ umount /mnt
 
 #
 mkfs.erofs --quiet -zlz4 -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hh/iso/airootfs.erofs /hh/
-xorriso -no_rc -as mkisofs -iso-level 3 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hh/iso/
+xorriso -no_rc -as mkisofs -iso-level 1 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 2048 -append_partition 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hh/iso/
 blkid /out/archiso-v-x86_64.iso
 sfdisk -l /out/archiso-v-x86_64.iso
