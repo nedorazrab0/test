11e6dd5d3909a54f81400eddd2cf2c3de3b8140c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-17 10:49:31 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -10,6 +10,7 @@ export SOURCE_DATE_EPOCH
 #SOURCE_DATE_EPOCH=$RANDOM
 
 set -ex
+cd /var
 pacman -Sy erofs-utils arch-install-scripts dosfstools xorriso python --noconfirm
 
 
@@ -20,26 +21,27 @@ cat << 'EOF' > /hh/so/etc/mkinitcpio.d/linux-zen.preset
 PRESETS=('hui')
 ALL_kver='/boot/vmlinuz-linux-zen'
 hui_config='/etc/mkinitcpio.conf.d/hui.conf'
-hui_image="/boot/initramfs-linux-zen.img"
+hui_image='/boot/initramfs-linux-zen.img'
 EOF
 
-pacstrap -cMG /hh/so base linux-zen mkinitcpio mkinitcpio-archiso &>/dev/null
+pacstrap -cMG /hh/so base linux-zen mkinitcpio mkinitcpio-archiso linux-firmware &>/dev/null
 mkdir -p /hh/so/etc/systemd/system-generators
 ln -sf /dev/null /hh/so/etc/systemd/system-generators/systemd-gpt-auto-generator
 
 # ESP
-espsize="$(du --block-size=1024 -cs /hh/so/boot | tail -n1 | awk '{print $1}')"
+espsize="$(du -cs /hh/so/boot | tail -n1 | awk '{print $1}')"
 
-espsize="$((espsize + 1024))"
-mkfs.fat -v -F32 -S512 -s1 -n 'ESP' -C esp.img "${espsize}"
-#mkfs.fat -v -F16 -S512 -s1 -n 'ESP' -C esp.img "${espsize}"
+dd if=/dev/zero of=./esp.img iflag=fullblock oflag=noatime ibs="${espsize}" obs=256K conv=fsync
+
+#espsize="$((espsize + 1024))"
+mkfs.fat -v -F32 -S512 -s1 -n 'ESP' ./esp.img
 mount esp.img /mnt
 
 mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
 cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi /mnt/EFI/BOOT/BOOTx64.EFI
 
 TZ=UTC printf -v iso_uuid '%(%F-%H-%M-%S-00)T' "$SOURCE_DATE_EPOCH"
-#iso_uuid=2cf777e5-cb87-473e-bf22-c6cf14d6f3fc
+iso_uuid=2cf777e5-cb87-473e-bf22-c6cf14d6f3fc
 cat << EOF > /mnt/loader/entries/a.conf
 title a
 linux /vmlinuz-linux-zen
@@ -54,7 +56,7 @@ umount /mnt
 rm -rf /hh/so/usr/share/{doc,man}
 
 #
-mkfs.erofs --quiet -zlzma -Efragments,dedupe,force-inode-extended,ztailpacking -C262144 -T0 -- /hhh/airootfs.erofs /hh/
-xorriso -no_rc -as mkisofs -iso-level 3 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hhh
+mkfs.erofs --quiet -zlzma -Efragments,dedupe,force-inode-extended,ztailpacking   -C1048576 -T0 -- /hhh/airootfs.erofs /hh/
+xorriso -no_rc -as mkisofs -iso-level 2 -rational-rock -volid HUI -appid 'Arch Linux baseline' -publisher 'Arch Linux <https://archlinux.org>' -preparer 'prepared by mkarchiso' -partition_offset 16 -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B esp.img -appended_part_as_gpt -no-pad -output /out/archiso-v-x86_64.iso /hhh
 blkid /out/archiso-v-x86_64.iso
 sfdisk -l /out/archiso-v-x86_64.iso
