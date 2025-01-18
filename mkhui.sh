46da8207e299e3a90fce446f68f4e7f5c31583ae
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-18 23:02:41 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -2,12 +2,17 @@
 #
 #
 
+set -ex
+umask 0077
+idir='/var/archiso-v/idir'
+odir='/var/archiso-v/odir'
+isodir='/var/archiso-v/iso'
+
+
 # poshol nahui dolbaeb kotoriy pridumal pihat datu v uuid
 [[ -v SOURCE_DATE_EPOCH ]] || printf -v SOURCE_DATE_EPOCH '%(%s)T' -1
-printf -v iso_uuid '%(%F-%H-%M-%S-00)T' "$SOURCE_DATE_EPOCH"
+printf -v iso_uuid '%(%F-%H-%M-%S-00)T' "${SOURCE_DATE_EPOCH}"
 
-set -ex
-cd /var
 pacman -Sy erofs-utils arch-install-scripts dosfstools xorriso python --noconfirm
 
 pkgs='amd-ucode
@@ -29,62 +34,60 @@ less
 nano
 zram-generator'
 
-mkdir -p ./hh/etc/mkinitcpio{,.conf}.d ./hhh ./hh ./out
+mkdir -p "${idir}/etc/mkinitcpio"{,.conf}.d "${odir} "${idir} "${isodir}"
 
-cat << EOF > ./hh/etc/mkinitcpio.conf.d/hui.conf
+cat << 'EOF' > "${idir}/etc/mkinitcpio.conf.d/hui.conf
 HOOKS=(base udev archiso_loop_mnt modconf archiso block filesystems)
-COMPRESSION="xz"
-COMPRESSION_OPTIONS=(-9e)
+COMPRESSION='xz'
+COMPRESSION_OPTIONS=(-9e -T0 -M100%)
 EOF
 
-cat << 'EOF' > ./hh/etc/mkinitcpio.d/linux-zen.preset
+cat << 'EOF' > "${idir}/etc/mkinitcpio.d/linux-zen.preset"
 PRESETS=('hui')
 ALL_kver='/boot/vmlinuz-linux-zen'
 hui_config='/etc/mkinitcpio.conf.d/hui.conf'
 hui_image='/boot/initramfs-linux-zen.img'
 EOF
 
-pacstrap -cMG ./hh ${pkgs} &>/dev/null
-mkdir -p ./hh/etc/systemd/system-generators
-ln -sf /dev/null ./hh/etc/systemd/system-generators/systemd-gpt-auto-generator
+pacstrap -cMG "${idir}" ${pkgs} &>/dev/null
+mkdir -p "${idir}/etc/systemd/system-generators"
+ln -sf /dev/null "${idir}/etc/systemd/system-generators/systemd-gpt-auto-generator"
 
 # ESP
-bootsize="$(du --block-size=1 -cs ./hh/boot \
+bootsize="$(du --block-size=1 -cs "${idir}/boot" \
   | tail -n1 | awk '{print $1}')"
 espsize="$((bootsize + 1*1024*1024))"
 
-dd if=/dev/zero of=./esp.img iflag=fullblock oflag=noatime ibs="${espsize}" count=1 obs=256K conv=fsync
+dd if=/dev/zero of="${isodir}/esp.img" iflag=fullblock oflag=noatime \
+  ibs="${espsize}" obs=256K count=1 conv=fsync
 
-mkfs.fat -v -F32 -S512 -s1 -n 'ESP' ./esp.img
-mount ./esp.img /mnt
+mkfs.fat -v -F32 -S512 -s1 -n 'ESP' "${isodir}/esp.img"
+mount "${isodir}/esp.img" /mnt
 
 mkdir -p /mnt/loader/entries /mnt/EFI/BOOT
 cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi /mnt/EFI/BOOT/BOOTx64.EFI
 
-#printf -v iso_uuid '%(%F-%H-%M-%S-00)T' "$SOURCE_DATE_EPOCH"
-cat << EOF > /mnt/loader/entries/a.conf
-title a
+cat << EOF > /mnt/loader/entries/archiso-zen.conf
+title ArchISO-V ZEN
 linux /vmlinuz-linux-zen
 initrd /amd-ucode.img
 initrd /initramfs-linux-zen.img
-options archisosearchuuid=$iso_uuid arch=/ archisobasedir=/
+options archisosearchuuid=${iso_uuid} arch=/ archisobasedir=/
 EOF
 
-mv ./hh/boot/* /mnt
+mv "${idir}/boot/"* /mnt
 find /mnt
 umount /mnt
 
-rm -rf ./hh/usr/share/{doc,man}
+rm -rf "${idir}/usr/share/"{doc,man}
 
 #
 mkfs.erofs -Efragments,dedupe,force-inode-extended,ztailpacking -T0 \
-  --quiet -zlzma,1,dictsize=8388608 -C1048576 ./hhh/airootfs.erofs ./hh
+  --quiet -zlzma,109,dictsize=8388608 -C1048576 "${odir}/airootfs.erofs" "${idir}"
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 xorriso -no_rc -temp_mem_limit 1024m -as mkisofs -iso-level 2 -rational-rock \
   -volid 'ARCHISO' -appid 'archiso-v' -preparer 'prepared by archiso-v' \
   -publisher 'arch-v <https://github.com/nedorazrab0/archiso-v>' \
   -append_partition 2 'C12A7328-F81F-11D2-BA4B-00A0C93EC93B' ./esp.img \
   -appended_part_as_gpt -partition_offset 16 -no-pad \
-  -output ./out/archiso-v-x86_64.iso ./hhh
-blkid ./out/archiso-v-x86_64.iso
-sfdisk -l ./out/archiso-v-x86_64.iso
+  -output "${isodir}/archiso-v.iso" "${odir}"
