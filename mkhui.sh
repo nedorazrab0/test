d1bd19ef8cb9b95a8f41547747513c725c7d05c2
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-19 02:59:08 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -50,7 +50,7 @@ hui_config='/etc/mkinitcpio.conf.d/hui.conf'
 hui_image='/boot/initramfs-linux-zen.img'
 EOF
 
-pacstrap -cMG "${idir}" ${pkgs} &>/dev/null
+pacstrap -cMGP "${idir}" ${pkgs} &>/dev/null
 mkdir -p "${idir}/etc/systemd/system-generators"
 ln -sf /dev/null "${idir}/etc/systemd/system-generators/systemd-gpt-auto-generator"
 
