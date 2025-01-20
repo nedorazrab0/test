48b67a19506bc3983a932331a678957c6b096a36
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-01-20 18:29:47 +0300

    Update mkhui.sh

--- mkhui.sh
+++ mkhui.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #
-#
+# Build an archiso
 
 set -ex
 umask 0077
 idir='/var/archiso-v/idir'
 odir='/var/archiso-v/odir'
 isodir='/var/archiso-v/iso'
-export LC_ALL='C.UTF-8'
 
 # poshol nahui dolbaeb kotoriy pridumal pihat datu v uuid
 [[ -v SOURCE_DATE_EPOCH ]] || printf -v SOURCE_DATE_EPOCH '%(%s)T' -1
 printf -v iso_uuid '%(%F-%H-%M-%S-00)T' "${SOURCE_DATE_EPOCH}"
 export SOURCE_DATE_EPOCH
 
-pacman -Sy erofs-utils arch-install-scripts dosfstools mtools xorriso python --noconfirm
+pacman -Sy arch-install-scripts mtools \
+  xorriso dosfstools erofs-utils --noconfirm
 
 pkgs='amd-ucode
 base
@@ -35,31 +35,28 @@ less
 nano
 zram-generator'
 
-mkdir -p "${idir}/etc/mkinitcpio"{,.conf}.d "${odir}" "${idir}" "${isodir}"
+mkdir -p "${idir}/etc/mkinitcpio"{,.conf}.d \
+  "${odir}" "${idir}" "${isodir}"
 
-cat << 'EOF' > "${idir}/etc/mkinitcpio.conf.d/hui.conf"
+cat << 'EOF' > "${idir}/etc/mkinitcpio.conf.d/isov.conf"
 HOOKS=(base udev archiso_loop_mnt microcode modconf archiso block filesystems)
 COMPRESSION='xz'
 COMPRESSION_OPTIONS=(-9e -T0 -M100%)
 EOF
 
 cat << 'EOF' > "${idir}/etc/mkinitcpio.d/linux-zen.preset"
-PRESETS=('hui')
+PRESETS=('isov')
 ALL_kver='/boot/vmlinuz-linux-zen'
-hui_config='/etc/mkinitcpio.conf.d/hui.conf'
-hui_image='/boot/initramfs-linux-zen.img'
+isov_config='/etc/mkinitcpio.conf.d/isov.conf'
+isov_image='/boot/initramfs-linux-zen.img'
 EOF
 
-pacstrap -cGP "${idir}" ${pkgs} &>/dev/null
+pacstrap -cGP "${idir}" ${pkgs} #&>/dev/null
 
-du -had 1 /usr
-du -had 1 /usr/lib
-exit
-
-rm -rvf /usr/include /usr/lib/lib{go,icudata}.so* /usr/share/locale /usr/share/info /usr/share/terminfo
 
 mkdir -p "${idir}/etc/systemd/system-generators"
-ln -sf /dev/null "${idir}/etc/systemd/system-generators/systemd-gpt-auto-generator"
+ln -sf /dev/null \
+  "${idir}/etc/systemd/system-generators/systemd-gpt-auto-generator"
 
 date +'archiso_v%d-%m-%y' \
   > "${idir}/etc/hostname"
@@ -74,7 +71,7 @@ rm -rf "${idir}/usr/share/"{doc,man}
 
 # ESP
 rm -f "${idir}/boot/amd-ucode.img"
-bootsize="$(du --block-size=1024 -cs "${idir}/boot" \
+bootsize="$(du -B1024 -s "${idir}/boot" \
   | tail -n1 | awk '{print $1}')"
 espsize="$((bootsize + 512))"
 
@@ -85,11 +82,11 @@ mount "${isodir}/esp.img" /mnt
 echo 'editor no' \
   > /var/loader.conf
   
-cat << EOF > /var/archiso-zen.conf
-title ArchISO-V ZEN
+cat << EOF > /var/arch9660-zen.conf
+title Arch9660 ZEN
 linux /vmlinuz-linux-zen
 initrd /initramfs-linux-zen.img
-options archisosearchuuid=${iso_uuid} arch=/ archisobasedir=/
+options archisosearchuuid=${iso_uuid} zswap.enabled=false copytoram=y arch=/ archisobasedir=/
 EOF
 
 mmd -i "${isodir}/esp.img" \
@@ -108,6 +105,11 @@ mcopy -i "${isodir}/esp.img" \
   /var/archiso-zen.conf '::/loader/entries'
 
 rm -rf "${idir}/"{boot,var,tmp}/*
+rm -rf /usr/include \
+  /usr/lib/lib{go,icudata}.so* /usr/share/info /usr/share/terminfo
+find "${idir}" -name '*.pacnew' \
+  -o -name '*.pacsave' -o -name '*.pacorig' -delete
+
 
 # EROFS compressed img
 mkfs.erofs -Efragments,dedupe,force-inode-extended,ztailpacking --quiet \
@@ -115,11 +117,8 @@ mkfs.erofs -Efragments,dedupe,force-inode-extended,ztailpacking --quiet \
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 # ISO file
 xorriso -no_rc -temp_mem_limit 1024m -as mkisofs -iso-level 2 -rational-rock \
-  -volid 'ARCHISO' -appid 'archiso-v' -preparer 'prepared by archiso-v' \
-  -publisher 'arch-v <https://github.com/nedorazrab0/archiso-v>' \
+  -volid 'ARCH9660' -appid 'arch9660' -preparer 'prepared by arch9660' \
+  -publisher 'Arch9660 <https://github.com/nedorazrab0/arch9660>' \
   -append_partition 2 'C12A7328-F81F-11D2-BA4B-00A0C93EC93B' "${isodir}/esp.img" \
   -appended_part_as_gpt -partition_offset 16 -no-pad \
-  -output "${isodir}/archiso-v.iso" "${odir}"
-
-echo '****'
-openssl sha256 "${isodir}/archiso-v.iso"
+  -output "${isodir}/arch9660.iso" "${odir}"
