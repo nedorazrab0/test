8dbc56ea5f520e8d1f955a72ab1826fa8f66b41e
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-09 20:35:00 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -ex
 
-cd /dev/shm
-dd if=/dev/zero of=./z bs=8G count=1 conv=fsync
+mkdir ./hui
+mount -t tmpfs -o size=9G ./hui
+cd ./hui
+
+dd if=/dev/zero of=./z ibs=9G obs=256K count=1 conv=fsync
 mkdir ./n
 
 mke2fs -t ext4 -Fq -I1024 -m0 -O "^has_journal,^metadata_csum,sparse_super2,\
 ^metadata_csum_seed,^resize_inode,stable_inodes,uninit_bg,extra_isize" ./z
 mount ./z ./n
 
-dd if=/dev/zero of=./n/h bs=7G count=1 conv=fsync
+dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
 
 umount ./z
 mkfs.xfs -f -i 'size=2048' ./z
 mount ./z ./n
 
-dd if=/dev/zero of=./n/h bs=7G count=1 conv=fsync
+dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
