56cb468393b9569647fb41dec7a926e1e2cef8ed
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-09 22:07:37 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -8,7 +8,7 @@ cd ./hui
 truncate -s 9G z
 mkdir ./n
 
-mkfs.btrfs -m single -n65536 --csum xxhash  ./z
+mkfs.btrfs -m single -n65536  ./z
 mount -o ssd,nodiscard,nobarrier,noatime ./z ./n
 
 dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
