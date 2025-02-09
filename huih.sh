b4c1d9f2d75a8b8c85305937ea6ffac1e3e5dd0b
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-09 20:26:58 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+set -ex
 
-for i in $(seq 2147483647); do
-  a=$((a + 1))
-done
+cd /dev/shm
+dd if=/dev/zero of=./z ibs=7.6G obs=256K count=1 conv=fsync
+mkdir ./n
 
-echo "${a}"
+mke2fs -t ext4 -Fq -I1024 -m0 -O "^has_journal,^metadata_csum,sparse_super2,\
+^metadata_csum_seed,^resize_inode,stable_inodes,uninit_bg,extra_isize" ./z
+mount ./z ./n
+
+dd if=/dev/zero of=./n/h ibs=7.5G obs=256K count=1 conv=fsync
+
+umount ./z
+mkfs.xfs -f -i 'size=2048' ./z
+mount ./z ./n
+
+dd if=/dev/zero of=./n/h ibs=7.5G obs=256K count=1 conv=fsync
