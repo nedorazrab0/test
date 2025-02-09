bd57583b4af734d3a9398fcc3a3bf3381fc28c94
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-09 20:32:26 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -2,17 +2,17 @@
 set -ex
 
 cd /dev/shm
-dd if=/dev/zero of=./z ibs=7500M obs=256K count=1 conv=fsync
+dd if=/dev/zero of=./z bs=7500M count=1 conv=fsync
 mkdir ./n
 
 mke2fs -t ext4 -Fq -I1024 -m0 -O "^has_journal,^metadata_csum,sparse_super2,\
 ^metadata_csum_seed,^resize_inode,stable_inodes,uninit_bg,extra_isize" ./z
 mount ./z ./n
 
-dd if=/dev/zero of=./n/h ibs=7G obs=256K count=1 conv=fsync
+dd if=/dev/zero of=./n/h bs=7G count=1 conv=fsync
 
 umount ./z
 mkfs.xfs -f -i 'size=2048' ./z
 mount ./z ./n
 
-dd if=/dev/zero of=./n/h ibs=7G obs=256K count=1 conv=fsync
+dd if=/dev/zero of=./n/h bs=7G count=1 conv=fsync
