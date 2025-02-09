cc587aa17e9bc413581e83b5d9e2d7459f0990d2
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-09 21:30:49 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -2,7 +2,7 @@
 set -ex
 
 mkdir ./hui
-mount -t tmpfs -o size=9G,mode=1777 hui ./hui
+#mount -t tmpfs -o size=9G,mode=1777 hui ./hui
 cd ./hui
 
 truncate -s 9G z
@@ -13,9 +13,8 @@ mount -o ssd,nodiscard,nodatacow,nodatasum,nobarrier,noatime ./z ./n
 
 dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
 
-modprobe null_blk
 umount ./z
-mkfs.xfs -i size=1024 -m crc=0 -l logdev=/dev/nullb1 -f ./z
+mkfs.xfs -i size=1024 -m crc=0 -f ./z
 mount ./z ./n
 
 dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
