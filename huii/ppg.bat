194e7cf3bbc36d9151c71c44a412f2f635e23bfc
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-13 22:14:53 +0300

    Add files via upload

---
+++ huii/ppg.bat
@@ -0,0 +1,208 @@
+@echo off
+color b
+chcp 437 > nul
+
+:PPGAY
+
+echo 	                         .cccc;;cc;';c.
+echo                       .,:dkdc:;;:c:,:d:.
+echo                      .loc'.,cc::::::,..,:.
+echo                    .cl;....;dkdccc::,...c;
+echo                   .c:,';:'..ckc',;::;....;c.
+echo                 .c:'.,dkkoc:ok:;llllc,,c,';:.
+echo                .;c,';okkkkkkkk:,lllll,:kd;.;:,.
+echo                co..:kkkkkkkkkk:;llllc':kkc..oNc
+echo              .cl;.,okkkkkkkkkkc,:cll;,okkc'.cO;
+echo              ;k:..ckkkkkkkkkkkl..,;,.;xkko:',l'
+echo             .,...';dkkkkkkkkkkd;.....ckkkl'.cO;
+echo          .,,:,.;oo:ckkkkkkkkkkkdoc;;cdkkkc..cd,
+echo       .cclo;,ccdkkl;llccdkkkkkkkkkkkkkkkd,.c;
+echo      .lol:;;okkkkkxooc::loodkkkkkkkkkkkko'.oc
+echo    .c:'..lkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.oc
+echo   .lo;,ccdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.c;
+echo ,dx:..;lllllllllllllllllllllllllllllllloc'...
+echo cNO;........................................
+timeout /nobreak /t 1 > nul
+
+echo 	                 .ckx;'........':c.
+echo              .,:c:c:::oxxocoo::::,',.
+echo             .odc'..:lkkoolllllo;..;d,
+echo             ;c..:o:..;:..',;'.......;.
+echo            ,c..:0Xx::o:.,cllc:,'::,.,c.
+echo            ;c;lkXXXXXXl.;lllll;lXXOo;':c.
+echo          ,dc.oXXXXXXXXl.,lllll;lXXXXx,c0:
+echo          ;Oc.oXXXXXXXXo.':ll:;'oXXXXO;,l'
+echo          'l;;OXXXXXXXXd'.'::'..dXXXXO;,l'
+echo          'l;:0XXXXXXXX0x:...,:o0XXXXk,:x,
+echo          'l;;kXXXXXXKXXXkol;oXXXXXXXO;oNc
+echo         ,c'..ckk0XXXXXXXXXX00XXXXXXX0:;o:.
+echo       .':;..:dd::ooooOXXXXXXXXXXXXXXXo..c;
+echo     .',',:co0XX0kkkxx0XXXXXXXXXXXXXXX0c..;l.
+echo   .:;'..oXXXXXXXXXXXXXXXXXXXXXXXXXXXXXko;';:.
+echo .cdc..:oOXXXXXXXXKXXXXXXXXXXXXXXXXXXXXXXo..oc
+echo :0o...:dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxo,.:,
+echo cNo........................................;'
+timeout /nobreak /t 1 > nul
+
+
+echo 	             .cc;.  ...  .;c.
+echo          .,,cc:cc:lxxxl:ccc:;,.
+echo         .lo;...lKKklllookl..cO;
+echo       .cl;.,;'.okl;...'.;,..';:.
+echo      .:o;;dkx,.ll..,cc::,..,'.;:,.
+echo      co..lKKKkokl.':lllo;''ol..;dl.
+echo    .,c;.,xKKKKKKo.':llll;.'oOxo,.cl,.
+echo    cNo..lKKKKKKKo'';llll;;okKKKl..oNc
+echo    cNo..lKKKKKKKko;':c:,'lKKKKKo'.oNc
+echo    cNo..lKKKKKKKKKl.....'dKKKKKxc,l0:
+echo    .c:'.lKKKKKKKKKk;....oKKKKKKo'.oNc
+echo      ,:.,oxOKKKKKKKOxxxxOKKKKKKxc,;ol:.
+echo      ;c..'':oookKKKKKKKKKKKKKKKKKk:.'clc.
+echo    ,dl'.,oxo;'';oxOKKKKKKKKKKKKKKKOxxl::;,,.
+echo   .dOc..lKKKkoooookKKKKKKKKKKKKKKKKKKKxl,;ol.
+echo   cx,';okKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKl..;lc.
+echo   co..:dddddddddddddddddddddddddddddddddl:;''::.
+echo   co..........................................."
+timeout /nobreak /t 1 > nul
+
+echo 	            .ccccccc.
+echo       .,,,;cooolccol;;,,.
+echo      .dOx;..;lllll;..;xOd.
+echo    .cdo,',loOXXXXXkll;';odc.
+echo   ,oo:;c,':oko:cccccc,...ckl.
+echo   ;c.;kXo..::..;c::'.......oc
+echo ,dc..oXX0kk0o.':lll;..cxxc.,ld,
+echo kNo.'oXXXXXXo'':lll;..oXXOd;cOd.
+echo KOc;oOXXXXXXo.':lol,..dXXXXl';xc
+echo Ol,:k0XXXXXX0c.,clc'.:0XXXXx,.oc
+echo KOc;dOXXXXXXXl..';'..lXXXXXd..oc
+echo dNo..oXXXXXXXOx:..'lxOXXXXXk,.:; ..
+echo cNo..lXXXXXXXXXOolkXXXXXXXXXkl;..;:.;.
+echo .,;'.,dkkkkk0XXXXXXXXXXXXXXXXXOxxl;,;,;l:.
+echo   ;c.;:''''':doOXXXXXXXXXXXXXXXXXXOdo;';clc.
+echo   ;c.lOdood:'''oXXXXXXXXXXXXXXXXXXXXXk,..;ol.
+echo   ';.:xxxxxocccoxxxxxxxxxxxxxxxxxxxxxxl::'.';;.
+echo   ';........................................;l'
+timeout /nobreak /t 1 > nul
+
+echo.
+echo         .;:;;,.,;;::,.
+echo      .;':;........'co:.
+echo    .clc;'':cllllc::,.':c.
+echo   .lo;;o:coxdlooollc;',::,,.
+echo .c:'.,cl,.'lc',,;;'......cO;
+echo do;';oxoc::l;;llllc'.';;'.';.
+echo c..ckkkkkkkd,;llllc'.:kkd;.':c.
+echo '.,okkkkkkkkc;llllc,.:kkkdl,cO;
+echo ..;xkkkkkkkkc,ccll:,;okkkkk:,cl,
+echo ..,dkkkkkkkkc..,;,'ckkkkkkkc;ll.
+echo ..'okkkkkkkko,....'okkkkkkkc,:c.
+echo c..ckkkkkkkkkdl;,:okkkkkkkkd,.',';.
+echo d..':lxkkkkkkkkxxkkkkkkkkkkkdoc;,;'..'.,.
+echo o...'';llllldkkkkkkkkkkkkkkkkkkdll;..'cdo.
+echo o..,l;'''''';dkkkkkkkkkkkkkkkkkkkkdlc,..;lc.
+echo o..;lc;;;;;;,,;clllllllllllllllllllllc'..,:c.
+echo o..........................................;'
+timeout /nobreak /t 1 > nul
+
+echo.
+echo            .,,,,,,,,,.
+echo          .ckKxodooxOOdcc.
+echo       .cclooc'....';;cool.
+echo      .loc;;;;clllllc;;;;;:;,.
+echo    .c:'.,okd;;cdo:::::cl,..oc
+echo   .:o;';okkx;';;,';::;'....,;,.
+echo   co..ckkkkkddk:,cclll;.,c:,:o:.
+echo   co..ckkkkkkkk:,cllll;.:kkd,.':c.
+echo .,:;.,okkkkkkkk:,cclll;.:kkkdl;;o:.
+echo cNo..ckkkkkkkkko,.;llc,.ckkkkkc..oc
+echo ,dd;.:kkkkkkkkkx;..;:,.'lkkkkko,.:,
+echo   ;c.ckkkkkkkkkkc.....;ldkkkkkk:.,'
+echo ,dc..'okkkkkkkkkxoc;;cxkkkkkkkkc..,;,.
+echo kNo..':lllllldkkkkkkkkkkkkkkkkkdcc,.;l.
+echo KOc,l;''''''';lldkkkkkkkkkkkkkkkkkc..;lc.
+echo xx:':;;;;,.,,...,;;cllllllllllllllc;'.;oo,
+echo cNo.....................................oc
+timeout /nobreak /t 1 > nul
+
+echo.
+echo.
+echo                    .ccccccc.
+echo                .ccckNKOOOOkdcc.
+echo             .;;cc:ccccccc:,::::,,.
+echo          .c;:;.,cccllxOOOxlllc,;ol.
+echo         .lkc,coxo:;oOOxooooooo;..:,
+echo       .cdc.,dOOOc..cOd,.',,;'....':c.
+echo       cNx'.lOOOOxlldOl..;lll;.....cO;
+echo      ,do;,:dOOOOOOOOOl'':lll;..:d:.'c,
+echo      co..lOOOOOOOOOOOl'':lll;.'lOd,.cd.
+echo      co.,dOOOOOOOOOOOo,.;llc,.,dOOc..dc
+echo      co..lOOOOOOOOOOOOc.';:,..cOOOl..oc
+echo    .,:;.'::lxOOOOOOOOOo:'...,:oOOOc..dc
+echo    ;Oc..cl'':llxOOOOOOOOdcclxOOOOx,.cd.
+echo   .:;';lxl''''':lldOOOOOOOOOOOOOOc..oc
+echo ,dl,.'cooc:::,....,::coooooooooooc'.c:
+echo cNo.................................oc
+timeout /nobreak /t 1 > nul
+
+echo.
+echo.
+echo.
+echo                         .cccccccc.
+echo                   .,,,;;cc:cccccc:;;,.
+echo                 .cdxo;..,::cccc::,..;l.
+echo                ,oo:,,:c:cdxxdllll:;,';:,.
+echo              .cl;.,oxxc'.,cc,.',;;'...oNc
+echo              ;Oc..cxxxc'.,c;..;lll;...cO;
+echo            .;;',:ldxxxdoldxc..;lll:'...'c,
+echo            ;c..cxxxxkxxkxxxc'.;lll:'','.cdc.
+echo          .c;.;odxxxxxxxxxxxd;.,cll;.,l:.'dNc
+echo         .:,''ccoxkxxkxxxxxxx:..,:;'.:xc..oNc
+echo       .lc,.'lc':dxxxkxxxxxxxdl,...',lx:..dNc
+echo      .:,',coxoc;;ccccoxxxxxxxxo:::oxxo,.cdc.
+echo   .;':;.'oxxxxxc''''';cccoxxxxxxxxxkxc..oc
+echo ,do:'..,:llllll:;;;;;;,..,;:lllllllll;..oc
+echo cNo.....................................oc
+timeout /nobreak /t 1 > nul
+
+echo.
+echo.
+echo                               .ccccc.
+echo                          .cc;'coooxkl;.
+echo                      .:c:::c:,;,,,;c;;,.'.
+echo                    .clc,',:,..:xxocc;...c;
+echo                   .c:,';:ox:..:c,,,,,,...cd,
+echo                 .c:'.,oxxxxl::l:.;loll;..;ol.
+echo                 ;Oc..:xxxxxxxxx:.,llll,....oc
+echo              .,;,',:loxxxxxxxxx:.,llll;.,;.'ld,
+echo             .lo;..:xxxxxxxxxxxx:.'cllc,.:l:'cO;
+echo            .:;...'cxxxxxxxxxxxxol;,::,..cdl;;l'
+echo          .cl;':;'';oxxxxxxxxxxxxx:....,cooc,cO;
+echo      .,,,::;,lxoc:,,:lxxxxxxxxxxxo:,,;lxxl;'oNc
+echo    .cdxo;':lxxxxxxc'';cccccoxxxxxxxxxxxxo,.;lc.
+echo   .loc'.'lxxxxxxxxocc;''''';ccoxxxxxxxxx:..oc
+echo occ'..',:cccccccccccc:;;;;;;;;:ccccccccc,.'c,
+echo Ol;......................................;l'
+timeout /nobreak /t 1 > nul
+
+echo.
+echo                               ,ddoodd,
+echo                          .cc' ,ooccoo,'cc.
+echo                       .ccldo;....,,...;oxdc.
+echo                    .,,:cc;.''..;lol;;,'..lkl.
+echo                   .dkc';:ccl;..;dl,.''.....oc
+echo                 .,lc',cdddddlccld;.,;c::'..,cc:.
+echo                 cNo..:ddddddddddd;':clll;,c,';xc
+echo                .lo;,clddddddddddd;':clll;:kc..;'
+echo              .,:;..:ddddddddddddd:';clll;;ll,..
+echo              ;Oc..';:ldddddddddddl,.,c:;';dd;..
+echo            .''',:lc,'cdddddddddddo:,'...'cdd;..
+echo          .cdc';lddd:';lddddddddddddd;.';lddl,..
+echo       .,;::;,cdddddol;;lllllodddddddlcodddd:.'l,
+echo      .dOc..,lddddddddlccc;'';cclddddddddddd;,ll.
+echo    .coc,;::ldddddddddddddl:ccc:ldddddddddlc,ck;
+echo ,dl::,..,cccccccccccccccccccccccccccccccc:;':xx,
+echo cNd.........................................;lOc
+timeout /nobreak /t 1 > nul
+
+goto PPGAY
