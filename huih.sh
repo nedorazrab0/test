65b275153eba7fa97b41e2f721ea7ce2a072541a
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-15 17:13:45 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -14,6 +14,6 @@ losetup /dev/loop0 ./z
 #zfs set mountpoint="$(realpath ./n)" test
 
 mkfs.btrfs -n65536 -f -m single --csum xxhash ./z
-mount -o compression=zstd:3,ssd,lazytime,commit=300,noatime,nodiscard,nobarrier /dev/loop0 ./n
+mount -o compress=zstd:3,ssd,lazytime,commit=300,noatime,nodiscard,nobarrier /dev/loop0 ./n
 
 dd if=/dev/urandom of=./n/h ibs=8G obs=256K count=1 conv=fsync
