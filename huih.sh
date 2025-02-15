54cee7f7017e296e9ae8729262fd38a4421c0b7c
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-15 16:53:08 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -9,12 +9,11 @@ truncate -s 9G z
 mkdir ./n
 losetup /dev/loop0 ./z
 
-zpool create -O acltype=posixacl  -O mountpoint="$(realpath ./n)" test /dev/loop0
+#zpool create -O acltype=posixacl  -O mountpoint="$(realpath ./n)" test /dev/loop0
 #-O compression=zstd
-zfs set mountpoint="$(realpath ./n)" test
+#zfs set mountpoint="$(realpath ./n)" test
 
-#mkfs.xfs -i size=1024 -m crc=0 ./z
-#mount /dev/loop0 ./n
+mkfs.xfs -i size=1024 -m crc=0 ./z
+mount /dev/loop0 ./n
 
-dd if=/dev/zero of=./n/h ibs=8G obs=256K count=1 conv=fsync
-openssl sha256 ./n/z #
+dd if=/dev/urandom of=./n/h ibs=8G obs=256K count=1 conv=fsync
