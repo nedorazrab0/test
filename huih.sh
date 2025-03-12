2023997aae2a8c0177ee940859328a2de3b87b90
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-03-12 09:20:58 +0300

    Update huih.sh

--- huih.sh
+++ huih.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir ./hui
-#mount -t tmpfs -o size=9G,mode=1777 hui ./hui
-cd ./hui
+rq(){
+  for rq in /sys/block/*/queue/rq_affinity; do
+    echo "$1" > "$rq"
+  done
 
-truncate -s 9G z
-mkdir ./n
-losetup /dev/loop0 ./z
+  rm -f z
+  sleep 1
+  dd if=/dev/zero of=z bs=10G count=1 conv=fsync
+  sleep 1
+}
 
-#zpool create -O acltype=posixacl -O compression=zstd -O mountpoint="$(realpath ./n)" test /dev/loop0
-
-#zfs set mountpoint="$(realpath ./n)" test
-
-mkfs.btrfs -n65536 -f -m single --csum xxhash ./z
-mount -o compress=zstd:3,ssd,lazytime,commit=300,noatime,nodiscard,nobarrier /dev/loop0 ./n
-
-dd if=/dev/urandom of=./n/h ibs=8G obs=256K count=1 conv=fsync
+rq 0
+rq 1
+rq 2
