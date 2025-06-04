1e7b3535d0eac5b1f05ef68fd2c43c6b015aa56e
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-06-04 12:11:12 +0000

    Create psm.py

---
+++ psm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+# smooth dim
+
+from time import sleep
+from subprocess import call, check_output
+from math import ceil
+
+sec = 30
+
+brightnow = int(check_output(["brightnessctl", "g"]))
+step = str(ceil(brightnow/sec))
+
+for i in range(n):
+    call(["brightnessctl", "-q", "-n10", "s", step + "-"])
+    sleep(1)
+  
